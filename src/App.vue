67b0801e184e743bb0f2dc6b171c60f535887f58
AlberTgarY <AlbertG2001@outlook.com>
2023-01-31 23:44:59 +0000

    feat: add axios and cheerio

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,75 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import {ref} from 'vue'
+import axios from 'axios'
+import cheerio, { type Cheerio } from 'cheerio'
+import { createDOMCompilerError } from '@vue/compiler-dom'
+
+const restaurantList = ref<String[]>([])
+const newRestaurantName = ref('');
+const AxiosInstance = axios.create(); // Create a new Axios Instance
+
+function log() {
+  const url = 'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
+  // Send an async HTTP Get request to the url
+
+    
+  // This is the structure of the player data we recieve
+  interface PlayerData {
+    rank: number; // 1 - 20 rank
+    name: string;
+    nationality: string;
+    goals: number;
+  }
+
+  // Send an async HTTP Get request to the url
+  AxiosInstance.get(url)
+    .then( // Once we have data returned ...
+      response => {
+        const html = response.data; // Get the HTML from the HTTP request
+        const $ = cheerio.load(html); // Load the HTML string into cheerio
+        const statsTable: Cheerio<any> = $('.statsTableContainer > tr'); // Parse the HTML and extract just whatever code contains .statsTableContainer and has tr inside
+        const topScorers: PlayerData[] = [];
+
+        statsTable.each((i, elem) => {
+          const rank: number = parseInt($(elem).find('.rank > strong').text()); // Parse the rank
+          const name: string = $(elem).find('.playerName > strong').text(); // Parse the name
+          const nationality: string = $(elem).find('.playerCountry').text(); // Parse the country
+          const goals: number = parseInt($(elem).find('.mainStat').text()); // Parse the number of goals
+          topScorers.push({
+            rank,
+            name,
+            nationality,
+            goals
+          })
+        })
+
+        console.log(topScorers);
+      }
+    )
+    .catch(console.error); // Error handling
+};
+
+
+
 </script>
 
 <template>
   <header>
-    <h1>My Website</h1>
+    <h1>My Website2</h1>
   </header>
   <main>
+    <form @submit.prevent="log">
+      <input id="restaurant-name"
+      v-model="newRestaurantName" type="text" />
+      <button type="submit">Click NE</button>
+    </form>
+    <ul>
+      <li v-for="rest in restaurantList" >
+        {{ rest }}
+      </li>
+    </ul>
     
   </main>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,5 @@ import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import './assets/main.css'
-import axios from 'axios';
-
-function fetchPage(url: string): Promise<string | undefined> {
-  const HTMLData = axios
-    .get(url)
-    .then(res => res.data)
-    .catch((error: AxiosError) => {
-      console.error(`There was an error with ${error.config.url}.`);
-      console.error(error.toJSON());
-    });
-
-  return HTMLData;
-}
 
 createApp(App).mount('#app')
