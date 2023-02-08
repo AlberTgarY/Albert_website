c26ba68c3cfdb67ba1f64bd5cca8ef37c062324a
AlberTgarY <AlbertG2001@outlook.com>
2023-02-08 02:04:06 +0000

    feat: dynamically update github repo

--- src/App.vue
+++ src/App.vue
@@ -6,48 +6,58 @@ import axios from 'axios'
 import cheerio, { type Cheerio } from 'cheerio'
 import { createDOMCompilerError } from '@vue/compiler-dom'
 
-const restaurantList = ref<String[]>([])
-const newRestaurantName = ref('');
+interface Repo {
+    name?: String
+    files: String[]
+}
+
+const RepoContent = ref<Repo[]>([]);
+const url = ref('');
 const AxiosInstance = axios.create(); // Create a new Axios Instance
 
 function log() {
-  const url = 'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
-  const url1 = 'https://github.com/AlberTgarY/SensatUrban_albert'
   // Send an async HTTP Get request to the url
-
-    
+  // const url = 'https://github.com/AlberTgarY/SensatUrban_albert'
+  
   // This is the structure of the player data we recieve
-  interface PlayerData {
-    name: string;
-  }
+  console.log(url);
 
   // Send an async HTTP Get request to the url
-    AxiosInstance.get(url1)
+  AxiosInstance.get(url.value)
     .then( // Once we have data returned ...
       response => {
         const html = response.data; // Get the HTML from the HTTP request
         const $ = cheerio.load(html); // Load the HTML string into cheerio
         const statsTable: Cheerio<any> = $(".Details-content--hidden-not-important.js-navigation-container.js-active-navigation-container.d-md-block"); // Parse the HTML and extract just whatever code contains .statsTableContainer and has tr inside
-        const statsTable1: Cheerio<any> = $(".js-active-navigation-container.d-md-block > div");
-        
-        const topScorers: PlayerData[] = [];
-        // for (let i = 0; i < statsTable1.length ; i++) {
-        //   const name: string = $(elem).find('div:nth-child('+'3'+') > div.flex-auto.min-width-0.col-md-2.mr-3 > span').text(); // Parse the name
-        //   topScorers.push({
-        //     name,
-        //   })
-        //   console.log ("Block statement execution no." + i);
-        // }
-        statsTable.each((i, elem) => {
-          for (let i = 0; i <= statsTable1.length; i++) {
-            const name: string = $(elem).find('div:nth-child('+i.toString()+') > div.flex-auto.min-width-0.col-md-2.mr-3 > span').text(); // Parse the name
-            topScorers.push({
-              name,
-            })
+        const fileDiv: Cheerio<any> = $(".js-active-navigation-container.d-md-block > div");
+        const repoNameDiv: Cheerio<any> = $(".px-md-4.px-lg-5");
+
+        let curFile = "";
+        let fileList: string[]= [];
+        // parse the repo name 
+        repoNameDiv.each((_, elem) => {
+          const temp =  $(elem).find('div > div > strong').text();
+          if(temp!=""){
+            curFile = temp;
+          }
+            
+        })
+        // Parse the files name
+        statsTable.each((_, elem) => {
+          for (let i = 0; i <= fileDiv.length; i++) {
+            const curFile: string = $(elem).find('div:nth-child('+i.toString()+') > div.flex-auto.min-width-0.col-md-2.mr-3 > span').text(); 
+            
+            if (curFile!=""){
+              fileList.push(curFile)
+            }
           }
         })
-        
-        console.log(topScorers);
+        RepoContent.value.push({
+          name : curFile,
+          files: fileList
+        })
+        //console.log(curFile);
+        console.log(RepoContent);
       }
       
     )
@@ -63,13 +73,16 @@ function log() {
   </header>
   <main>
     <form @submit.prevent="log">
-      <input id="restaurant-name"
-      v-model="newRestaurantName" type="text" />
-      <button type="submit">Click NE</button>
+      <input v-model="url" type="text" class="form-control" placeholder="github repository url" aria-label="Username" aria-describedby="basic-addon1">
+      <button  class="btn btn-outline-primary" type="submit">Click ME</button>
     </form>
-    <ul>
-      <li v-for="rest in restaurantList" >
-        {{ rest }}
+    <ul class="list-group">
+      <li class="list-group-item" v-for="repo in RepoContent" >
+        {{ repo.name }}
+        <li class="list-group-item" v-for="f in repo.files" >
+          {{ f }}
+        </li>
+
       </li>
     </ul>
     
